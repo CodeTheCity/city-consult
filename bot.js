7b692be6346798e2464d71706a60c3b6b1192a18
Gavin Barnett <gavbarnett@gmail.com>
2017-02-25 15:45:24 +0000

    demo
    Faking it working.

--- bot.js
+++ bot.js
@@ -10,10 +10,10 @@ Main bot logic for teh bot is stored here. Needs Server.js to run.
     //set-up function used when bot is first called
     var stdout = [];
     stdout[0] = "Welcome. There are several topics that are currently under consultation.";
-    stdout[1] = "  A. The city should pedestrianise Union Street.";
-    stdout[2] = "  B. The city should fund daycare.";
-    stdout[3] = "  C. The city should build a dedicated tramline.";
-    stdout[4] = "Please state your views on one topic, and then we can continue.";
+    stdout[1] = "It would be great is you could help us with the following question:"
+    stdout[2] = "  Do you think Aberdeen City Council should pedestrianise Union Street?"
+    stdout[3] = "  Please Responed 'yes' or 'no'"
+
     return stdout;
   },
 
@@ -24,9 +24,15 @@ Main bot logic for teh bot is stored here. Needs Server.js to run.
     //Split the stdin into an UPPERCASE array divided by "space"
     var splitstring = stdin.toUpperCase().split(" ");
     //bot logic goes here
-
+    for (var i in splitstring) {
+      if ((splitstring[i] == "YES")||(splitstring[i] == "NO")) {
+        stdout[0] = "Ok, great. Can you give a short reason why this would be a good idea for the city?";
+      } else {
+        stdout[0] = "Fantastic! Can you think of another reason for this initiative.";
+      }
+    }
     //demo response
-    stdout[0] = "I am a demo response!";
+    //stdout[0] = "I am a demo response!";
 
     return stdout;
   }
