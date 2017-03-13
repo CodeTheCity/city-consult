a311c9ae391a17ad76f723fc3bc90a03b24101df
azwyner <azwyner@abdn.ac.uk>
2017-03-13 22:57:03 +0000

    revised the bot statements

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ Main bot logic for teh bot is stored here. Needs Server.js to run.
     stdout[1] = "It would be great if you could help us with the following discussion:"
     stdout[2] = "  Aberdeen City Council is considering pedestrianising Union Street?"
     stdout[3] = "We'd like to hear your thoughts on this."
-    stdout[4] = " Firstly, Can you please decribe how you feel about the current situation on Union Street."
+    stdout[4] = " Firstly, Can you please tell us what you think about the current situation on Union Street."
     return stdout;
   },
 
@@ -30,7 +30,7 @@ Main bot logic for teh bot is stored here. Needs Server.js to run.
       case "currentSituation":
         stdout[0] = "positiveResult"
         stdout[1][userstage] = stdin;
-        stdout[2] = "What would be the positive impact of this action?"
+        stdout[2] = "What should be done about this situation?"
         break;
       case "positiveResult":
         stdout[0] = "negativeResult"
@@ -47,7 +47,7 @@ Main bot logic for teh bot is stored here. Needs Server.js to run.
         stdout[1][userstage] = stdin;
         console.log(stdout[1]);
         mongo.save(stdout[1]); //push data to database
-        stdout[2] = "Thanks, thats been really helpful!"
+        stdout[2] = "Thanks, that's been really helpful!"
         stdout[3] = "Bye now, have a nice day. (referesh to do this again)"
         break;
       default:
